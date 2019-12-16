0c8ef090465674e713c55d4a0f6cf5dd69ffb549
Y.Arimitsu <yoshikazu_arimitsu@albert2005.co.jp>
2019-12-16 03:29:17 +0000

    ubuntu18.04

--- .bin/setup.sh
+++ .bin/setup.sh
@@ -1,2 +1,18 @@
 #/bin/sh
 
+sudo apt install -y peco direnv tig python3-venv python3-dev python-pip
+pip2 install --user powerline-shell
+
+git config --global user.name Y.Arimitsu
+git config --global user.email yoshikazu_arimitsu@albert2005.co.jp
+
+## Python3.6 venv
+python3 -m venv ~/.py36
+
+## Node.js LTS
+. ~/.nvm/nvm.sh
+nvm install node --lts
+
+echo '. ./dotfiles/.bashrc' >> ~/.bashrc
+
+cp -prf .config ~
