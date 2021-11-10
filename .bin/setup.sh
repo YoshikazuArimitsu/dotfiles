106a8a38370ae7cc60ce06bab609c1701b271e22
Y.Arimitsu <yoshikazu_arimitsu@albert2005.co.jp>
2021-11-10 23:28:51 +0900

    Python/node のローカルインストール卒業

--- .bin/setup.sh
+++ .bin/setup.sh
@@ -1,26 +1,16 @@
 #/bin/sh
 
-sudo apt install -y peco direnv tig python3-venv python3-dev python-pip
-# pip2 install --user powerline-shell
+sudo apt install -y peco direnv tig
 
 git config --global user.name Y.Arimitsu
 git config --global user.email yoshikazu_arimitsu@albert2005.co.jp
-
-## Python3.6 venv
-# python3 -m venv ~/.py36
-
-## Node.js LTS
-. ~/.nvm/nvm.sh
-nvm install node --lts
 
 echo '. ~/dotfiles/.bashrc' >> ~/.bashrc
 
 cp -prf ~/dotfiles/.config ~
 cp -prf ~/dotfiles/.emacs ~
 
-# 
 mkdir ~/bin
-
 cd ~/bin
 
 # install powerline-go
